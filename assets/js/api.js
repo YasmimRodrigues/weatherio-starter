631d3ea367d812f9582098ea0f73eae58673c16b
YasmimRodrigues <yasmimrbm25@gmail.com>
2025-10-15 12:05:19 -0300

    Update README and project improvements

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,45 @@
 /**
  * @license MIT
  * @fileoverview All api related stuff like api_key, api request etc.
- * @copyright codewithsadee 2023 All rights reserved
+   @copyright codewithsadee 2023 All rights reserved
  * @author codewithsadee <mohammadsadee24@gmail.com>
+ * @modified by yasmimrbm25 <yasmimrbm25@gmail.com>
  */
+
+'use strict';
+
+const api_key = "f8ea21df0bd17fe7991c3d5987cecdef";
+
+/**
+ * Fetch data fro, server
+ * @param {string} URL API url
+ * @param {Function} callback callback
+*/
+
+export const fetchData = function (URL, callback) {
+    fetch(`${URL}&appid=${api_key}`)
+      .then(res => res.json())
+      .then(data => callback(data))
+
+}
+
+export const url = {
+    currentWeather(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
+    },
+    forecast(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
+    },
+    airPollution(lat, lon) {
+        return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
+    },
+    reverseGeo(lat, lon) {
+        return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+    },
+    /**
+     * @param {string} query Search query e.g.: "London, New York"
+     */
+    geo(query) {
+        return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
+    }
+}
